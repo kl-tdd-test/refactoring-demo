32b982dc1ad57a6c26e63251818fdc6c44cce69a
kl-tdd-test <kl-tdd-test@mail.ru>
2017-07-11 15:54:59 +0300

    Refactored for DI

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -3,6 +3,8 @@ package com.scrumtrek.simplestore;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 /**
  * Created by User on 10.07.2017.
  */
@@ -18,26 +20,43 @@ public class CustomerTest {
     public void shouldStatementContainWhenStatement() {
         String name = "Vasya";
         Customer customer = new Customer(name);
-        Assert.assertEquals(name, customer.getName());
-
         String statement = customer.Statement();
+
+//       Movie movie1 = new Movie("movie1", PriceCodes.Regular);
+        Movie movie1 = mock(Movie.class);
+        when(movie1.getPriceCode()).thenReturn(PriceCodes.Regular);
+        when(movie1.getTitle()).thenReturn("movie1");
+
+        Movie movie2 = mock(Movie.class);
+        when(movie2.getPriceCode()).thenReturn(PriceCodes.Childrens);
+        when(movie2.getTitle()).thenReturn("movie2");
+
+        Movie movie3 = mock(Movie.class);
+        when(movie3.getPriceCode()).thenReturn(PriceCodes.NewRelease);
+        when(movie3.getTitle()).thenReturn("movie3");
+
+        Rental rent1 = mock(Rental.class);
+        when(rent1.getDaysRented()).thenReturn(3);
+        when(rent1.getMovie()).thenReturn(movie1);
+
+        Rental rent2 = mock(Rental.class);
+        when(rent2.getDaysRented()).thenReturn(1);
+        when(rent2.getMovie()).thenReturn(movie2);
+
+        Rental rent3 = mock(Rental.class);
+        when(rent3.getDaysRented()).thenReturn(4);
+        when(rent3.getMovie()).thenReturn(movie3);
+
+        customer.addRental(rent1);
 
         Assert.assertTrue(statement.startsWith("Rental record for " + name + "\n"));
         //System.out.println(customer.Statement());
         Assert.assertTrue(statement.endsWith("Amount owed is " + 0.0 + "\n" +
-            "You earned " + 0 + " frequent renter points."));
+                "You earned " + 0 + " frequent renter points."));
 
-        Movie movie1 = new Movie("movie1", PriceCodes.Regular);
-        Movie movie2 = new Movie("movie2", PriceCodes.Childrens);
-        Movie movie3 = new Movie("movie3", PriceCodes.NewRelease);
-
-        Rental rent1 = new Rental(movie1, 3);
-        Rental rent2 = new Rental(movie2, 1);
-        Rental rent3 = new Rental(movie3, 4);
-
-        customer.addRental(rent1);
         //System.out.println(customer.Statement());
         Assert.assertTrue(customer.Statement().contains("\tmovie1\t3.5"));
+        verify(movie1).getTitle();
         Assert.assertTrue(customer.Statement().endsWith("Amount owed is " + (2+1.5) + "\n" +
                 "You earned " + 1 + " frequent renter points."));
 
